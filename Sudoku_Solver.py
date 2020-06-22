034ef2bc2855088eb74b3485c2b6954c997d2e40
Sahil Hemnani <hemnanisahil777@gmail.com>
2020-06-22 21:49:27 +0530

    Added two new functions (print_board, find_empty)
    print_board:
    it will print the board in the sudoku style.
    
    find_empty:
    it will the find the empty entries in the puzzle and return a tupple (i,j)==(row, coloum).

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -11,3 +11,24 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+"""Function to Print Board in Sudoku style"""
+def print_board(bo):
+    for i in range(len(bo)):
+        if i%3==0 and i!=0:
+            print("------------------------")
+        for j in range(len(bo[0])):
+            if j%3==0 and j!=0:
+                print(" | ", end="")
+            if j==8:
+                print(str(bo[i][j]))
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+print_board(board)
+
+"""Function to find the index of empty box in the puzzle"""
+def find_empty(bo):
+    for i in range(leb(bo)):
+        for j in range(len(bo[o])):
+            if bo[i][j]==0:
+                return (i,j) #row,coloum
