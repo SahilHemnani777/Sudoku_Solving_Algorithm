ff365d3c929fee13c0be7187dccb43b4fd0d8ad0
Sahil Hemnani <hemnanisahil777@gmail.com>
2020-06-22 22:12:15 +0530

    Added a function which Validates the empty box in the puzzles boxses with numbers
    function name :valid(bo, num, pos)
    bo=board, num=the number which we want to check, pos=output of the check_empty function(tuple)
    it check all Rows, Coloums, Boxses(3x3)
    return True if number is valid
    return False if number is nit valid

--- Sudoku_Solver.py
+++ Sudoku_Solver.py
@@ -11,6 +11,31 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+"""Function to check if the number we pass is valid or not
+return True if the number is valid
+return Flase if the number is not valid"""
+
+def valid(bo, num, pos):
+    #check Rows
+    for i in range(len(bo[o])):
+        if bo[pos[0]][i]==num and i!=pos[1]:
+            return False
+    #check coloum
+    for i in range(len(bo[0])):
+        if bo[i][bo[1]]==num and i!=bo[0]:
+            return False
+    #check box(3x3)
+    box_x=pos[1]//3
+    box_y=pos[0]//3
+
+    for i in range(box_x*3, box_x*3 + 3):    #itterating through rows
+        for j in range(box_y*3, box_y*3 + 3):
+            if bo[i][j]==num and (i, j)!=pos:
+                return False
+    return True
+
+        
 """Function to Print Board in Sudoku style"""
 def print_board(bo):
     for i in range(len(bo)):
